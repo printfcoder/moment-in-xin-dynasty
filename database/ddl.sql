eb16f13f8369f319924c927f79da614274b9d0eb
shu xian <i@shux.me>
2022-08-29 12:53:53 +0800

    add some event

--- database/ddl.sql
+++ database/ddl.sql
@@ -1,18 +1,23 @@
 -- SQLite
-CREATE table people (
-  id int PRIMARY AUTOINCREMENT UNIQUE,
-  name varchar(20) not null,
-  birthday varchar(20) not null,
-  deathday varchar(20) not null
-)
+CREATE table people
+(
+    id       integer primary key autoincrement,
+    name     varchar(20) not null,
+    birthday varchar(20) not null,
+    deathday varchar(20) not null
+);
 
-CREATE table people_occupation ( --人物职业时间表 
-  id int(8) UNIQUE AUTOINCREMENT,
-  people_id int not null,
-  people_name varchar(20) not null,
-  occupation_name   varchar(20) not null, -- 职业名
-  occupation_begin_time varchar(20), -- 职业开始时间
-  occupation_end_time varchar(20), -- 职业结束时间
-  posthumous_title  varchar(20) -- 谥号
-  temple_title varchar(20) -- 庙号
-)
+create table people_occupation  -- 人物职业时间表 
+(
+    id                    INTEGER
+        primary key autoincrement,
+    people_id             INT         not null,
+    people_name           varchar(20) not null,
+    occupation_name       varchar(20) not null,  -- 职业名
+    occupation_begin_time varchar(20), -- 职业开始时间
+    occupation_end_time   varchar(20), -- 职业结束时间
+    posthumous_title      varchar(20), -- 谥号
+    temple_title          varchar(20), -- 庙号
+    reference             TEXT 
+);
+
